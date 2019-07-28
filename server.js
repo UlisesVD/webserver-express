82683a0a4a87de2eeb45bfbf333cf041ec1f6f9c
Ulises Vera Delgado <uli_vra@hotmail.com>
2019-07-27 19:30:09 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+hbs.registerHelper('getAnio', ()=> new Date().getFullYear())
+app.get('/', (req, res) => {
+  res.render('home',{
+      nombre: "Ulises"
+  });
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+ 
+app.listen(port, ()=>{
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
